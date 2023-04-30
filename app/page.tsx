38d495c3fb9dcb3feae136771d53344364b9489c
Pijus <p.pozera@gmail.com>
2023-04-30 19:32:44 +0300

    Tailwind setup

--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,5 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    return <div>Hello airbnb</div>;
+    return <div className="text-rose-500 text-2xl">Hello airbnb</div>;
 }
