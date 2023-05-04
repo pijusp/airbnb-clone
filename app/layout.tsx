aa9174686710d9caf83fb3f458b490ce0b92964f
Pijus <p.pozera@gmail.com>
2023-05-04 15:19:38 +0300

    Auth UI WIP

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 export const metadata = {
     title: "Airbnb",
@@ -17,7 +19,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={font.className}>
-                <Navbar />
+                <ClientOnly>
+                    <Modal isOpen />
+                    <Navbar />
+                </ClientOnly>
                 {children}
             </body>
         </html>
