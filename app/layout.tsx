873e5c77ef0d93909cf5afd5063da665fd38ccce
Pijus <p.pozera@gmail.com>
2023-05-10 12:38:33 +0300

    Register footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 
 import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 export const metadata = {
     title: "Airbnb",
@@ -21,6 +22,7 @@ export default function RootLayout({
         <html lang="en">
             <body className={font.className}>
                 <ClientOnly>
+                    <ToasterProvider />
                     <RegisterModal />
                     <Navbar />
                 </ClientOnly>
