7d46e6fa9a89f636a958bb5841a568aca778a7dc
Pijus <p.pozera@gmail.com>
2023-06-12 13:32:53 +0300

    Listing favourite button

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -2,6 +2,7 @@
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
+import useFavorite from "../hooks/useFavorite";
 
 interface HeartButtonProps {
     listingId: string;
@@ -11,8 +12,11 @@ const HeartButton: React.FC<HeartButtonProps> = ({
     listingId,
     currentUser,
 }) => {
-    const hasFavorited = false;
-    const toggleFavorite = () => {};
+    const { hasFavorited, toggleFavorite } = useFavorite({
+        listingId,
+        currentUser,
+    });
+
     return (
         <div
             onClick={toggleFavorite}
@@ -24,7 +28,9 @@ const HeartButton: React.FC<HeartButtonProps> = ({
             />
             <AiFillHeart
                 size={24}
-                className="hasFavorited ? 'fill-rose-500 : fill-neutral-500/70"
+                className={
+                    hasFavorited ? "fill-rose-500" : "fill-neutral-500/70"
+                }
             />
         </div>
     );
