d25aa0c215b260932745b98586a168ac593e7bd2
Pijus <p.pozera@gmail.com>
2023-06-14 15:28:35 +0300

    "Reservations" page

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -69,7 +69,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                                     label="My favorites"
                                 />
                                 <MenuItem
-                                    onClick={() => {}}
+                                    onClick={() => router.push("/reservations")}
                                     label="My reservations"
                                 />
                                 <MenuItem
